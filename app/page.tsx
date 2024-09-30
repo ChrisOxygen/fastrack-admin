a31a948ce753d2c9ed9b10c67b782b6a547b0fda
Christopher Okafor <chrisoxygen1234@gmail.com>
2024-09-30 23:16:27 +0100

    fixed login loading

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,5 @@
 import LoginForm from "@/components/LoginForm";
-import { getServerSession } from "next-auth";
-import Image from "next/image";
-import Link from "next/link";
-import { redirect } from "next/navigation";
 
-export default async function Home() {
-  const session = await getServerSession();
-
-  if (session) {
-    redirect("/dashboard");
-  }
-  return (
-    <main className=" w-full  h-screen bg-white grid place-items-center">
-      <div className=" w-full max-w-[400px] min-h-[400px] p-10 rounded-xl border border-gray-400 flex flex-col items-center ">
-        <Link
-          href="/"
-          className="flex gap-2 text-center font-syne text-xl font-bold mb-4"
-        >
-          <Image
-            src="/fastrack-black.png"
-            alt="fastrack"
-            width={200}
-            height={10}
-          />
-        </Link>
-        <LoginForm />
-      </div>
-    </main>
-  );
+export default function Home() {
+  return <LoginForm />;
 }
--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -6,6 +6,9 @@ import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { SubmitHandler, useForm } from "react-hook-form";
 import LoadingSpinner from "./LoadingSpinner";
+import Link from "next/link";
+import Image from "next/image";
+import useFetchAdmin from "@/hooks/useFetchAdmin";
 
 type LoginInputs = {
   email: string;
@@ -15,6 +18,8 @@ type LoginInputs = {
 function LoginForm() {
   const queryClient = useQueryClient();
   const router = useRouter();
+
+  const { session, isPending, isError, data } = useFetchAdmin();
   const {
     register,
     handleSubmit,
@@ -61,58 +66,83 @@ function LoginForm() {
     mutate(data);
   };
 
+  if (submitFormPending || isPending) {
+    return <LoadingSpinner />;
+  }
+
+  if (session) {
+    router.push("/dashboard");
+  }
+
   return (
-    <section className=" w-full flex flex-col items-center gap-2 font-dm_sans text-black">
-      <p className=" text-4xl font-bold">Welcome Back!</p>
-      <span className=" mb-4">Log in to continue.</span>
-      <form action="" className=" flex w-full flex-col gap-3">
-        <div className="flex w-full flex-col gap-2">
-          <label htmlFor="email" className=" font-bold capitalize">
-            Email
-          </label>
-          <input
-            type="email"
-            id="email"
-            placeholder="Enter your email"
-            className=" w-full px-3 py-2 border border-gray-400 rounded-md text-siteHeadingDark"
-            {...register("email", { required: "This field is required" })}
-          />
-          {errors.email && (
-            <span className=" italic text-red-500 self-end mt-[-5px] mr-1">
-              {errors.email.message}
-            </span>
-          )}
-        </div>
-        <div className="flex w-full flex-col gap-2">
-          <label htmlFor="password" className=" font-bold capitalize">
-            Password
-          </label>
-          <input
-            type="password"
-            id="password"
-            placeholder="Enter your password"
-            className="w-full px-3 py-2 border border-gray-400 rounded-md text-siteHeadingDark"
-            {...register("password", { required: "This field is required" })}
-          />
-          {errors.password && (
-            <span className=" italic text-red-500 self-end mt-[-5px] mr-1">
-              {errors.password.message}
-            </span>
-          )}
-        </div>
-        <button
-          type="submit"
-          className="px-3 mt-4 py-2 w-full rounded-md bg-siteHeadingDark text-white text-xl capitalize font-bold"
-          onClick={(e) => {
-            console.log("clicked");
-            e.preventDefault();
-            handleSubmit(onSubmit)();
-          }}
+    <main className=" w-full  h-screen bg-white grid place-items-center">
+      <div className=" w-full max-w-[400px] min-h-[400px] p-10 rounded-xl border border-gray-400 flex flex-col items-center ">
+        <Link
+          href="/"
+          className="flex gap-2 text-center font-syne text-xl font-bold mb-4"
         >
-          Log in
-        </button>
-      </form>
-    </section>
+          <Image
+            src="/fastrack-black.png"
+            alt="fastrack"
+            width={200}
+            height={10}
+          />
+        </Link>
+        <section className=" w-full flex flex-col items-center gap-2 font-dm_sans text-black">
+          <p className=" text-4xl font-bold">Welcome Back!</p>
+          <span className=" mb-4">Log in to continue.</span>
+          <form action="" className=" flex w-full flex-col gap-3">
+            <div className="flex w-full flex-col gap-2">
+              <label htmlFor="email" className=" font-bold capitalize">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Enter your email"
+                className=" w-full px-3 py-2 border border-gray-400 rounded-md text-siteHeadingDark"
+                {...register("email", { required: "This field is required" })}
+              />
+              {errors.email && (
+                <span className=" italic text-red-500 self-end mt-[-5px] mr-1">
+                  {errors.email.message}
+                </span>
+              )}
+            </div>
+            <div className="flex w-full flex-col gap-2">
+              <label htmlFor="password" className=" font-bold capitalize">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Enter your password"
+                className="w-full px-3 py-2 border border-gray-400 rounded-md text-siteHeadingDark"
+                {...register("password", {
+                  required: "This field is required",
+                })}
+              />
+              {errors.password && (
+                <span className=" italic text-red-500 self-end mt-[-5px] mr-1">
+                  {errors.password.message}
+                </span>
+              )}
+            </div>
+            <button
+              type="submit"
+              className="px-3 mt-4 py-2 w-full rounded-md bg-siteHeadingDark text-white text-xl capitalize font-bold"
+              onClick={(e) => {
+                console.log("clicked");
+                e.preventDefault();
+                handleSubmit(onSubmit)();
+              }}
+            >
+              Log in
+            </button>
+          </form>
+        </section>
+      </div>
+    </main>
   );
 }
 
