e07696eab79388548201f1c6fdaa50855d263085
Christopher Okafor <chrisoxygen1234@gmail.com>
2024-10-23 06:01:27 +0100

    add shadcn and new database file structure

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,41 +9,75 @@ const config: Config = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    screens: {
-      xsm: "440px",
-      // => @media (min-width: 640px) { ... }
-      sm: "664px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "880px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "1156px",
-      // => @media (min-width: 1024px) { ... }
-
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-
-      grid1364: "1364px",
-
-      "2xl": "1536px",
-      // => @media (min-width: 1536px) { ... }
-    },
-    extend: {
-      fontFamily: {
-        syne: ["var(--syne)"], // Adds a new `font-syne` class
-        dm_sans: ["var(--dm-sans)"],
-      },
-      colors: {
-        siteGreen: "#004838",
-        siteLemon: "#e2fb6c",
-        siteHeadingDark: "#1A202C",
-        siteBg: "#ebede8",
-        siteTextDark: "#333f3c",
-      },
-    },
+  	screens: {
+  		xsm: '440px',
+  		sm: '664px',
+  		md: '880px',
+  		lg: '1156px',
+  		xl: '1280px',
+  		grid1364: '1364px',
+  		'2xl': '1536px'
+  	},
+  	extend: {
+  		fontFamily: {
+  			syne: ["var(--syne)"],
+  			dm_sans: ["var(--dm-sans)"]
+  		},
+  		colors: {
+  			siteGreen: '#004838',
+  			siteLemon: '#e2fb6c',
+  			siteHeadingDark: '#1A202C',
+  			siteBg: '#ebede8',
+  			siteTextDark: '#333f3c',
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  darkMode: "class",
-  plugins: [nextui()],
+  darkMode: ["class", "class"],
+  plugins: [nextui(), require("tailwindcss-animate")],
 };
 export default config;
